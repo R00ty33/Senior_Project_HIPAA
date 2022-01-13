0fe3bf3dda21e50db6eb5297c456a2b3f5800273
Nicholas Rudolph <n.rudolph2011@gmail.com>
2022-01-13 13:07:45 -0500

    second commit

---
+++ src/main/java/com/HIPAA/SeniorProject/HipaaApplication.java
@@ -0,0 +1,13 @@
+package com.HIPAA.SeniorProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HipaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HipaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HIPAA/SeniorProject/HipaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HIPAA.SeniorProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HipaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
